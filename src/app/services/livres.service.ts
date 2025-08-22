afbe5100dfaf3074b8150f76d47bfac66c6a1b61
yohjimcode <yohjimcode@gmail.com>
2025-08-23 00:34:41 +0200

    deploy aws

--- src/app/services/livres.service.ts
+++ src/app/services/livres.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Livre } from '../models/livre.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environnement/environnement';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,6 @@ export class LivresService {
   constructor(private http: HttpClient) { }
 
   getAllLivres(): Observable<Livre[]> {
-  return this.http.get<Livre[]>(`${environment.apiUrl}/livres/all`);
+    return this.http.get<Livre[]>(`${environment.apiUrl}/livres/all`);
   }
 }
