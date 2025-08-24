b4d014eb52de218a9b6d21ae61f047adbd7bed2b
yohjimcode <yohjimcode@gmail.com>
2025-08-24 22:54:07 +0200

    Modif pour sécurité back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bibliotheque-rec.eu-west-3.elasticbeanstalk.com'
+  apiUrl: 'http://bibliotheque-rec.eu-west-3.elasticbeanstalk.com/api'
 };
