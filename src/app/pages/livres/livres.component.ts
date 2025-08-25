0549713e4212b93cc648d06424741b7908f57a74
yohjimcode <yohjimcode@gmail.com>
2025-08-26 00:27:06 +0200

    Ajout champs de recherche

--- src/app/pages/livres/livres.component.ts
+++ src/app/pages/livres/livres.component.ts
@@ -2,10 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Livre } from '../../models/livre.model';
 import { LivresService } from '../../services/livres.service';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-livres',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,     
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule
+  ],
   templateUrl: './livres.component.html',
   styleUrl: './livres.component.scss'
 })
@@ -19,5 +29,15 @@ export class LivresComponent implements OnInit {
     this.livresService.getAllLivres().subscribe(data => {
       this.livres = data;
     });
+  }
+
+  zoneRecherche: string = '';
+
+  get livresFiltres(): Livre[] {
+    return this.livres.filter(livre =>
+      livre.titre.toLowerCase().includes(this.zoneRecherche.toLowerCase()) ||
+      livre.auteur.nom.toLowerCase().includes(this.zoneRecherche.toLowerCase()) ||
+      livre.auteur.prenom.toLowerCase().includes(this.zoneRecherche.toLowerCase())
+    );
   }
 }
